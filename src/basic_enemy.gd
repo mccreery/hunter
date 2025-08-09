c97ef7d661ec48c8fa95f953590a07e0e563231f
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2025-08-09 20:30:29 +0100

    Add basic chase enemy

---
+++ src/basic_enemy.gd
@@ -0,0 +1,22 @@
+extends CharacterBody3D
+
+@export var speed := 2.0
+@export var target: Node3D
+
+
+func _process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	var direction := Plane(Vector3.UP) \
+			.project(target.transform.origin - transform.origin) \
+			.normalized()
+
+	var horz_velocity := direction * speed
+	velocity.x = horz_velocity.x
+	velocity.z = horz_velocity.z
+
+
+func _physics_process(delta: float) -> void:
+	move_and_slide()
