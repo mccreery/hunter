05ed1e735440b7038a6e73207930d73997b8b6f3
BavoM <bavo.mispelaere@hotmail.be>
2025-08-09 19:18:07 +0200

    Adding in placeholder sprites + start collision

--- src/player.gd
+++ src/player.gd
@@ -44,8 +44,8 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _process(delta: float) -> void:
 	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
